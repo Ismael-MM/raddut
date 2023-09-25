26e371655fed42add8bdb44e57f23f70f1bd1d2d
ismael <ismontelongo@outlook.com>
2023-09-25 22:12:09 +0200

    A6 Ejercicio 1. Más sobre rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Core\Number;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nombre/{name?}', function(?string $name = null){
+    return $name;
+});
+
+Route::get('/nombre/{name?}', function(?string $name = 'lucas'){
+    return $name;
+});
+
+Route::post('/nombreP/{name?}', function(?string $name = null){
+    return $name;
+});
+
+Route::post('/apellido/{name?}', function(?string $name = null){
+    return $name;
+});
+
+Route::get('/apellido/{name?}', function(?string $name = null){
+    return $name;
+});
+
+Route::get('/numeros/{numeros}', function (string $numeros) {
+    return $numeros;
+})->where('numeros', '[0-9]+');
+
+Route::get('/test/{letra}/{numeros}', function (string $letra, string $numeros) {
+    return ($letra + $numeros);
+})->where('numeros' => '[0-9]+', 'letra' => '[A-Z][a-z]+');
