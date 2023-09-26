29d8725c5d7978784bae68f5881f8539e1b890f8
Ismael <ismaelmontelongomon@gmail.com>
2023-09-26 20:04:19 +0200

    DPL cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Raddut</title>
+        <title>Raddut 0.2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
