246833c5ec524b979a255aab81ef8f9047b75670
Ismael <ismaelmontelongomon@gmail.com>
2023-10-23 19:12:30 +0100

    A13 fix ejercicio 1

--- app/Http/Controllers/CommunityLinkController.php
+++ app/Http/Controllers/CommunityLinkController.php
@@ -16,7 +16,7 @@ class CommunityLinkController extends Controller
     public function index(Channel $channel = null)
     {
         if ($channel != null) {
-            $links = CommunityLink::where('approved', true, 'and')->where('channel_id', $channel['id'])->latest('updated_at')->paginate(25);
+            $links = $channel->CommunityLinks()->where('approved', true)->latest('updated_at')->paginate(25);
         }else{
             $links = CommunityLink::where('approved', true)->latest('updated_at')->paginate(25);
         }
