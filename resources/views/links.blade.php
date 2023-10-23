926c0e8bda2d831c1cb6516034678ca6eaf9c785
Ismael <ismaelmontelongomon@gmail.com>
2023-10-23 20:32:17 +0100

    A14 Ejercicio 1. Formulario para votar

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,21 +1,27 @@
 <header>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/post.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/post.css') }}">
 </header>
 
 
 
 @if(count($links) == 0)
-                <p>Aún no hay contribuciones aprobadas</p>
+<p>Aún no hay contribuciones aprobadas</p>
 @endif
 @foreach ($links as $link)
-            <li>
-                <a href="{{ $link->link }}" target="_blank">
-                    {{ $link->title }}
-                </a>
-                <span class="label label-default" style="background: {{ $link->channel->color }}">
-                    <a class="text-decoration-none" href="/community/{{ $link->channel->slug }}"> {{ $link->channel->title }}</a>
-                </span>
-                <span>Votos: {{$link->users()->count()}}</span>
-                <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
-            </li>
+<li>
+    <a href="{{ $link->link }}" target="_blank">
+        {{ $link->title }}
+    </a>
+    <span class="label label-default" style="background: {{ $link->channel->color }}">
+        <a class="text-decoration-none" href="/community/{{ $link->channel->slug }}"> {{ $link->channel->title }}</a>
+    </span>
+    <form method="POST" action="/votes/{{ $link->id }}">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-secondary" {{ Auth::guest() ? 'disabled' : '' }}>
+            {{$link->users()->count()}}
+        </button>
+    </form>
+    <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
+
+</li>
 @endforeach
