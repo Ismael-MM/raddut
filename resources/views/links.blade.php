c560c550849bf415e6d9d54fd07316f09e464a38
Ismael <ismaelmontelongomon@gmail.com>
2023-10-23 18:40:42 +0100

    A13 Ejercicio 3. Relaciones

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -15,6 +15,7 @@
                 <span class="label label-default" style="background: {{ $link->channel->color }}">
                     <a class="text-decoration-none" href="/community/{{ $link->channel->slug }}"> {{ $link->channel->title }}</a>
                 </span>
+                <span>Votos: {{$link->users()->count()}}</span>
                 <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}</small>
             </li>
 @endforeach
