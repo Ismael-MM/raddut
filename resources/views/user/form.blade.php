01aa9feab3bac45a2ff63d5120add33bd72ed433
Ismael <ismaelmontelongomon@gmail.com>
2023-11-10 18:29:44 +0000

    Ejercicio 2. CRUD generator

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -12,6 +12,11 @@
             {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
+            {{ Form::label('password') }}
+            {{ Form::text('password', $user->password, ['class' => 'form-control' . ($errors->has('password') ? ' is-invalid' : ''), 'placeholder' => 'Password']) }}
+            {!! $errors->first('password', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
             {{ Form::label('trusted') }}
             {{ Form::text('trusted', $user->trusted, ['class' => 'form-control' . ($errors->has('trusted') ? ' is-invalid' : ''), 'placeholder' => 'Trusted']) }}
             {!! $errors->first('trusted', '<div class="invalid-feedback">:message</div>') !!}
